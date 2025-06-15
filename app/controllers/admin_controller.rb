5afadf23bd03ecc5f0d34fde5b4be446ad888e6d
hiroshi11112344 <hikikaeru0303@icloud.com>
2025-06-15 12:29:31 +0900

    一旦

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,40 @@
+class AdminController < ApplicationController
+  def login
+  end
+
+  def login_post
+    username = params[:username]
+    password = params[:password]
+
+    # ここで管理者認証を行う（例：Adminモデルがある場合）
+    admin = Admin.find_by(username: username)
+
+
+    if admin && admin.authenticate(password)
+      session[:admin_id] = admin.id
+      redirect_to home_show_path
+    else
+      render :login, status: :unprocessable_entity
+    end
+  end
+
+  def logout
+    session[:admin_id] = nil
+    redirect_to home_show_path
+  end
+
+  def new
+    @admin = Admin.find_by(id: session[:admin_id])
+  end
+
+
+  def create
+    @post = Post.new(params.require(:post).permit(:image1, :content1, :image2, :content2, :image3, :content3))
+    if @post.save
+      redirect_to admin_new_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+end
