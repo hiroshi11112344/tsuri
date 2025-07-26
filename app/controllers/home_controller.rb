19965e1db527cc5d290686c77cf8334b24a04939
hiroshi11112344 <hikikaeru0303@icloud.com>
2025-07-26 19:29:01 +0900

    ブログ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def show
     @admin = Admin.find_by(id: session[:admin_id]) if session[:admin_id]
+    @posts = Post.all.order(created_at: :desc)
   end
 end
